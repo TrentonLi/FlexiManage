8638ba719dd54d88f3e8c14a8ca5e3e032d6c8cb
168028 <18264495117@163.com>
2025-03-16 09:57:24 +0800

    home页架构调整

--- frontend/src/Container/index.vue
+++ frontend/src/Container/index.vue
@@ -40,35 +40,19 @@ const menuOptions: MenuOption[] = [
 </script>
 
 <template>
-  <n-layout>
-    <n-layout-header>
-      <Header :title="'导航栏'" />
-    </n-layout-header>
-    <n-layout has-sider>
-      <n-layout-sider content-style="padding: 24px;">
-        <Menu :menuOptions="menuOptions" />
-      </n-layout-sider>
-      <n-layout-content content-style="padding: 24px;">
-        视图窗口
-        <RouterView />
-      </n-layout-content>
-    </n-layout>
-    <n-layout-footer>底部</n-layout-footer>
-  </n-layout>
+  <!--导航栏-->
+  <Header class="header" :title="'导航栏'"/>
+  <!--视图窗-->
+  <div class="Viewport">
+    <div class="leftMenu">
+      <Menu :menu-options="menuOptions"/>
+    </div>
+    <div class="rightContent">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.n-layout-header,
-.n-layout-footer {
-  background: rgba(128, 128, 128, 0.2);
-  padding: 24px;
-}
-
-.n-layout-sider {
-  background: rgba(128, 128, 128, 0.3);
-}
-
-.n-layout-content {
-  background: rgba(128, 128, 128, 0.4);
-}
+@import "index.scss";
 </style>
